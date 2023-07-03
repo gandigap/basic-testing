1fe6cd21954271f64d68266b718c87158b35301e
Ihar Berasneu <iberesnev@alfabank.ru>
2023-07-03 21:50:20 +0300

    feat(*): add test for snapshot-testing and remove test for table

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,12 +1,10 @@
 import { simpleCalculator, Action } from './index';
 
 const testCases = [
-  { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 1, b: 1, action: Action.Add, expected: 2 },
   { a: 1, b: 1, action: Action.Subtract, expected: 0 },
-
+  { a: 1, b: 1, action: Action.Multiply, expected: 1 },
+  { a: 1, b: 1, action: Action.Divide, expected: 1 },
   { a: 1, b: 1, action: Action.Exponentiate, expected: 1 },
   { a: 1, b: 2, action: '', expected: null },
   { a: 'qw', b: 'qw', action: Action.Exponentiate, expected: null },
